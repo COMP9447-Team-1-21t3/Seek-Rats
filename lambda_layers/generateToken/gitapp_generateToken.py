2636ec3ab1dedcf38553e9ce3645bd8abe5ed899
z5257531 <62084453+z5257531@users.noreply.github.com>
2021-11-12 02:25:24 +1100

    Generating token functions

---
+++ lambda_layers/generateToken/gitapp_generateToken.py
@@ -0,0 +1,71 @@
+import jwt
+import requests
+import time
+from cryptography.hazmat.backends import default_backend
+from gitapp_generateToken_helpers import get_parameter
+
+cert_str = get_parameter("gitapp_PKey").get("Parameter").get("Value")
+app_id = get_parameter("gitapp_appID").get("Parameter").get("Value")
+
+def generate_jwt_headers():
+	"""
+		Summary: Generates the headers for a JWT token that will expire in 3 mins
+	"""
+	seconds= int(time.time())
+	cert_bytes= cert_str.encode()
+
+	private_key = default_backend().load_pem_private_key(cert_bytes, None)
+
+	payload = {
+		# issued at time, 60 seconds in the past to allow for clock drift
+		'iat': seconds - 60,
+		# JWT expiration time (3 minute maximum)
+		'exp': seconds + (3 * 60),
+		# GitHub App's identifier
+		'iss': app_id
+	}
+
+	a_jwt = jwt.encode(payload, private_key, algorithm='RS256')
+
+	headers = {
+		"Authorization": f"Bearer {a_jwt}", 
+		"Accept": "application/vnd.github.machine-man-preview+json"
+	}
+	return headers
+
+def generate_token_header(token):
+	headers = {
+		"Authorization": f"token {token}",
+        "Accept": "application/vnd.github.machine-man-preview+json"
+	}
+	return headers
+
+def generate_token(owner, repo):
+	"""
+	"""
+	headers=generate_jwt_headers()
+	response = requests.get(
+		f'https://api.github.com/repos/{owner}/{repo}/installation',
+		headers=headers
+	)
+	acc_tok_url = response.json()['access_tokens_url']
+	response = requests.post(acc_tok_url, headers=headers)
+	token = response.json()['token']
+	return token
+
+if __name__=="__main__":
+	#"https://api.github.com/app",
+	#"https://api.github.com/installation/repositories",
+	#"https://api.github.com/app/installations/20623405/access_tokens",
+	token = generate_token("COMP9447-Team-1-21t3", "Seek-Rats")
+	print (
+		f"token = {token}"
+	)
+
+	response = requests.get(
+		"https://api.github.com/installation/repositories",
+		headers=generate_token_header(token)
+	)
+	print (f"Status Code = {response.status_code}")
+	print(f"repo_id = {response.json()['repositories'][0]['id']}")
+	print(f"org_id = {response.json()['repositories'][0]['owner']['id']}")
