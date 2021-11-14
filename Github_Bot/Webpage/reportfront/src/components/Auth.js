535b81c6e2d908d642f80ef14362a60b08e33d78
wontonz-1 <danielsworkemail123@gmail.com>
2021-11-14 14:42:19 +1100

    added oauth github code

--- Github_Bot/Webpage/reportfront/src/components/Auth.js
+++ Github_Bot/Webpage/reportfront/src/components/Auth.js
@@ -4,23 +4,46 @@ import React, { useEffect, useState } from "react";
 import Report from './Report';
 
 function Auth() {
-    const [authenticated, setAuth] = useState(0);
+    const queryString = require('query-string');
+    const [repo_url, setURL] = useState("") //Get from query string
+    const [is_repo_valid, setValidCheck] = useState(false)
+    const parsed = queryString.parse(window.location.search);
 
+    const client_id = "bafc9bb95ef83e08ded6" //get from config
+    const redirect_uri = "http://localhost:3000" //Get it from config
 
-    if (authenticated === 1) {
-        console.log(authenticated);
+    if (parsed[repo_url]) {
+        setURL(repo_url);
+        setValidCheck(true);
+        console.log(repo_url);
+    }
+    
+    var auth_url = "https://github.com/login/oauth/authorize?client_id="
+    auth_url = auth_url.concat(client_id)
+    auth_url = auth_url.concat("&redirect_uri=")
+    auth_url = auth_url.concat(redirect_uri)
+    console.log(auth_url)
+
+    if (parsed["code"]) {
         return (
             <div>
-                <Report/>
+                <Report auth_url_json={parsed}/>
             </div>
         )
-    } else {
+    }
+     else {
         return (
+            
             <div>
-                <h1
-                    onClick={() => setAuth(1)}
-                > yo </h1>
-            </div>
+                {console.log(window.location.search)}
+                {is_repo_valid ? 
+                <a href={auth_url.concat(window.location.search)}>Login with Github</a> : <a>Need Repo Query</a>
+                }      
+                <a href={auth_url.concat(window.location.search)}>Login with Github</a>
+            </div>  
+             
+
+
         )
     }
     
--- Github_Bot/Webpage/reportfront/src/components/Report.js
+++ Github_Bot/Webpage/reportfront/src/components/Report.js
@@ -5,36 +5,87 @@ import url from './config.txt';
 import update_url from './config2.txt';
 import axios from "axios";
 
-function Report() {
+function Report({auth_url_json}) {
 
     // const [secrets, setSecrets] = useState([{"secret":"test", "type":"Unknown Secret","location":"line 2 app.py", "code_location": "minecraft_seed = -test", "id":"1"}, 
     // {"secret":"test", "type":"API Secret","location":"line 10 app.py", "code_location": "api_key= test", "id":"2"}])
-    const [secrets, setSecrets] = useState([])
-    const [user, setUser] = useState("9999999")
-    
-    const commit_id = "asdasd"
-    const sha = "Asdasda"
-    const date = "asdasd"
-    
 
+    const [token, setToken] = useState("")
+    const [ready, setReady] = useState(false)
+    const [error, setError] = useState(false)
+
+    const [repoURL, setURL] = useState(auth_url_json['code'])
+
+    console.log('passed correctly!')
+    console.log(auth_url_json)
+
+    // const sendForm = (event) => {
+    //   const formData = new FormData(event.currentTarget);
+    //   event.preventDefault();
+    //   console.log(Array.from(formData.entries()))
+    //   fetch(update_url).then(r => r.text()).then(text => {
+
+    //     const body = { body: Array.from(formData.entries()), commit_sha : sha, user : user};
+    //     const headers = { 
+    //         "Content-Type": "application/json"
+    //     };
+
+    //     axios.post(text.concat("/"), body, { headers })
+    //         .then(response => console.log(response));
+    //   });
+    // }
+    const [secrets, setSecrets] = useState([])
     useEffect(() => {
       const getData = () => {
-         fetch(url)
+         fetch(url) // Fetch Lambda url to request from 
           .then(r => r.text()) 
           .then(async text => {
             try {
-              let result = await axios.get(text.concat("/"));
+              
+              // Send code + repo_url as a post request to server
+              const body = {code : auth_url_json['code'], repo_url : auth_url_json['repo_url']};
+              const headers = { 
+                "Content-Type": "application/json"
+              };
 
-              setSecrets(result.data);
+              //TODO: change url back to text.concat("/") in live
+              let result = await axios.post("http://localhost:5000/report", body, headers);
               console.log(result);
-              console.log(result.data);
+              const data = (result.data)['body'];
+              const token_data = (result.data)['token'];
+              setSecrets(JSON.parse(data));
+              setToken(token_data);
+              setURL(auth_url_json['repo_url']);
+              setReady(true)
             } catch (error) {
               console.log(error);
+              setError(error);
             }
       });
       };
       getData();
     }, []);  
+
+
+    // const [secrets, setSecrets] = useState([])
+    // useEffect(() => {
+    //   const getData = () => {
+    //      fetch(url)
+    //       .then(r => r.text()) 
+    //       .then(async text => {
+    //         try {
+    //           let result = await axios.get(text.concat("/")); 
+
+    //           setSecrets(result.data);
+    //           console.log(result);
+    //           console.log(result.data);
+    //         } catch (error) {
+    //           console.log(error);
+    //         }
+    //   });
+    //   };
+    //   getData();
+    // }, []);  
     
 
     const sendForm = (event) => {
@@ -43,47 +94,50 @@ function Report() {
       console.log(Array.from(formData.entries()))
       fetch(update_url).then(r => r.text()).then(text => {
 
-        const body = { body: Array.from(formData.entries()), commit_sha : sha, user : user};
+        const body = { body: Array.from(formData.entries()), repo_url : repoURL, token : token};
         const headers = { 
             "Content-Type": "application/json"
         };
 
-        axios.post(text.concat("/"), body, { headers })
+        axios.post(text.concat("http://localhost:5000/"), body, { headers }) //TODO: change to text.concat("/") when live
             .then(response => console.log(response));
       });
     }
-
-    return (
-      <div>
-
-        <form onSubmit={(event) => sendForm(event)}>
-  
-          <h2> Secrets Report</h2>
-          <input type="hidden" name="id" value={commit_id}/>
-          <input type="hidden" name="sha" value={sha}/>
-  
-          
-          <p>Secrets report {date}</p>
-  
-          {secrets.map(secret => (
-          <div>
-          <h2>{secret.id} {secret.type} found in {secret.location}</h2>
-          <a> {secret.code_location} </a>
-          <ul>
-              <p><input type="radio" value="hub" name={secret.id} required></input> To Hub </p>
-              <p><input type="radio" value="allow" name={secret.id} required></input> To Allow </p>
-          </ul>
-          </div>
-          ))}
-  
-          <p>*All boxes must be selected to send</p>
-          
-          <button type="submit"> Submit </button>
-          </form>
-        
-      </div>
-    );
-  }
+    if (error === true) {
+      <p>Authorization Error</p>
+    } else {
+      return (
+        <div>
+          {ready ?
+          <form onSubmit={(event) => sendForm(event)}>
+    
+            <h2> Secrets Report</h2>
+            <input type="hidden" name="id" value={repoURL}/>
+    
+            <p>Secrets report</p>
+            
+            {secrets.map(secret => (
+            <div>
+            <h2>{secret.id}. {secret.type} found in {secret.location}</h2>
+            <a> {secret.code_location} </a>
+            <ul>
+                <p><input type="radio" value="hub" name={secret.id} required></input> To Hub </p>
+                <p><input type="radio" value="allow" name={secret.id} required></input> To Allow </p>
+            </ul>
+            </div>
+            ))}
+    
+            <p>*All boxes must be selected to send</p>
+            
+            <button type="submit"> Submit </button>
+            </form>
+            : <a>loading...</a>
+            
+            }
+        </div>
+      );
+    }
+}
   
   export default Report;
   
