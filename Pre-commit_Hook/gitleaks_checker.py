04837b4bf1bf502565e2b77d8cb1d621dc3d14cc
Badrivishal Iyengar <badrivishaliyengar@gmail.com>
2021-11-17 21:06:26 +1100

    Added API Integration for allowlist

--- Pre-commit_Hook/gitleaks_checker.py
+++ Pre-commit_Hook/gitleaks_checker.py
@@ -1,6 +1,25 @@
+# from _typeshed import NoneType
 import os
 import subprocess
 import json
+from requests import get
+import urllib.request
+from urllib.parse import quote
+
+
+def get_values(repo_url):
+    baseURL = "https://cl2tu6os57.execute-api.ap-southeast-2.amazonaws.com/beta"
+    url = f"{baseURL}/allowlist/get_list/"
+    data = {
+        'repo_url': quote(repo_url)
+    }
+    response = get(url, params=data)
+
+    if response.status_code != 200:
+        print("Server Error")
+    else:
+        return response.json()
+        
 
 def get_all_secrets(report_obj):
     all_secrets = []
@@ -15,7 +34,7 @@ def get_repo_url(report_obj):
 
 def checker(new_secrets):
     if (len(new_secrets)!=0):
-        print("Hardcoded secrets detected in your commit, use 'git commit --no-verify instead'")
+        print("Hardcoded secrets detected, consider fixing or  use 'git commit --no-verify instead'")
         return 1
     elif (len(new_secrets)==0):
         print(" No secrets detected, Commit Successful'")
@@ -23,17 +42,14 @@ def checker(new_secrets):
 
 
 def main():
-    loc = os.getcwd()
-    print('This is the current directory:',loc)
-
+    loc = os.getcwd() 
     process = subprocess.run(["gitleaks","-v", "--path="+ loc, "--unstaged"], capture_output= True , text= True)
 
     report_out = process.stdout
     report_err = process.stderr
-
+    new_secrets = []
     if("No leaks found" in report_err or "repository does not exist" in report_err):
-        print("No secrets in commit, Commit Successful")
-        return 0
+        val = checker(new_secrets)
 
     else:
         loc = []
@@ -48,24 +64,28 @@ def main():
         report_out = '[' + report_out + ']'
         report_obj = json.loads(report_out)
 
-        url = get_repo_url(report_obj)
+        repo_url = get_repo_url(report_obj)
     
-        detected_secrets = []
         detected_secrets = get_all_secrets(report_obj)
-        # print(detected_secrets)
 
-        api_return_val = ("string", ("term",("no_val", "no_val")))
+        api_return_val = get_values(repo_url)
+        
+        my_results = []
 
-        my_results = [(api_return_val[1][1])]
-        print(my_results)
-        new_secrets = []
+        if("whitelist" in api_return_val.keys()):
+
+            whitelist = api_return_val.whitelist
+
+            for i in range(len(whitelist)):
+                my_results += [whitelist[i][1].term]
+
+        
         for i in detected_secrets:
             if i not in my_results:
                 new_secrets.append(i)
-        
-        print(new_secrets)
-        val = checker(new_secrets)
-        return val
+       
+    val = checker(new_secrets)
+    return val
 
 if __name__ == '__main__':
     raise SystemExit(main())
