8cd0492c26dca5ea7aaca25eacdc9b1c4c7a8103
Badrivishal Iyengar <badrivishaliyengar@gmail.com>
2021-11-17 21:15:01 +1100

    Revert "Merge pull request #13 from COMP9447-Team-1-21t3/pre-commit-hook"
    This reverts commit 5ede9428ad4da0f04be70b3d4d610b4298868653, reversing
    changes made to b5db4f8cce4971f0dc9c6bdad80ec2cf962a4834.

--- Pre-commit_Hook/gitleaks_checker.py
+++ Pre-commit_Hook/gitleaks_checker.py
@@ -1,91 +1,28 @@
-# from _typeshed import NoneType
 import os
 import subprocess
-import json
-from requests import get
-import urllib.request
-from urllib.parse import quote
+import sys
 
-
-def get_values(repo_url):
-    baseURL = "https://cl2tu6os57.execute-api.ap-southeast-2.amazonaws.com/beta"
-    url = f"{baseURL}/allowlist/get_list/"
-    data = {
-        'repo_url': quote(repo_url)
-    }
-    response = get(url, params=data)
-
-    if response.status_code != 200:
-        print("Server Error")
-    else:
-        return response.json()
-        
-
-def get_all_secrets(report_obj):
-    all_secrets = []
-    for i in range(len(report_obj)):
-        secret = report_obj[i]['offender']
-        all_secrets.append(secret)
-    return all_secrets
-
-def get_repo_url(report_obj):
-    url = report_obj[0]['repoURL']
-    return url
-
-def checker(new_secrets):
-    if (len(new_secrets)!=0):
-        print("Hardcoded secrets detected, consider fixing or  use 'git commit --no-verify instead'")
-        return 1
-    elif (len(new_secrets)==0):
-        print(" No secrets detected, Commit Successful'")
+def checker(report_err, report_out):
+    if not report_err:
         return 0
 
+    if report_err:
+        print("Hardcoded secrets detected in your commit, use 'git commit --no-verify instead'")
+        return 1
 
 def main():
-    loc = os.getcwd() 
+    loc = os.getcwd()
+    print('This is the current directory:',loc)
+
     process = subprocess.run(["gitleaks","-v", "--path="+ loc, "--unstaged"], capture_output= True , text= True)
+
+    process
 
     report_out = process.stdout
     report_err = process.stderr
-    new_secrets = []
-    if("No leaks found" in report_err or "repository does not exist" in report_err):
-        val = checker(new_secrets)
+    return_val = checker(report_err, report_out)
 
-    else:
-        loc = []
-        for i in range(len(report_out)):
-            if(report_out[i]=="}"):
-                loc+=[i]
-
-        for i in loc:
-            if i!=loc[-1]:
-                report_out = report_out[:i+1]+','+report_out[i+1:]
-
-        report_out = '[' + report_out + ']'
-        report_obj = json.loads(report_out)
-
-        repo_url = get_repo_url(report_obj)
-    
-        detected_secrets = get_all_secrets(report_obj)
-
-        api_return_val = get_values(repo_url)
-        
-        my_results = []
-
-        if("whitelist" in api_return_val.keys()):
-
-            whitelist = api_return_val.whitelist
-
-            for i in range(len(whitelist)):
-                my_results += [whitelist[i][1].term]
-
-        
-        for i in detected_secrets:
-            if i not in my_results:
-                new_secrets.append(i)
-       
-    val = checker(new_secrets)
-    return val
+    return return_val
 
 if __name__ == '__main__':
     raise SystemExit(main())
