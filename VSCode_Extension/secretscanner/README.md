b040704b731b5829a9224bad8a86d896363fe1c8
Edward Gauld <edwardbgauld@gmail.com>
2021-11-21 19:58:08 +1300

    Enabled integration with allow list API and changed how the extension is configured

--- VSCode_Extension/secretscanner/README.md
+++ VSCode_Extension/secretscanner/README.md
@@ -11,3 +11,10 @@ When you hover over a highlighted secret with your cursor, information about the
 To try this extension out locally, in this directory, first run `npm install`\
 Then press F5 to enter debug mode\
 Create or open a file, press `cmd + Shift + p` and run the command 'Scan for Secrets' in the dropdown
+
+## How to configure
+The 'allowlist_url' should contain your organisation's allowlist url to fetch the allowlist\
+It can be extended to recognise more secret types.\
+In the `src/config.json` file, add more entries to the rules array.\
+'regex' is the regular expression to detect the secret type.\
+'description' will be shown when the user is hovering over the secret.
--- VSCode_Extension/secretscanner/src/extension.ts
+++ VSCode_Extension/secretscanner/src/extension.ts
@@ -1,14 +1,16 @@
 // The module 'vscode' contains the VS Code extensibility API
 import * as vscode from 'vscode';
+import axios from 'axios';
 
-export function activate(context: vscode.ExtensionContext) {
+export async function activate(context: vscode.ExtensionContext) {
 
 	const fs = require('fs');
 	const path = require('path');
-	const toml = require('toml');
-	const config = toml.parse(fs.readFileSync(path.join(__dirname, 'regex.toml'), 'utf-8'));
+	const config = JSON.parse(fs.readFileSync(path.join(__dirname, 'config.json'), 'utf-8'));
 	const rules = config.rules;
-		
+	const allow_list = await getAllowList();
+	// const allow_list = ['AKIAIOSFODNN7EXAMPLX'];
+	
 	let timeout: NodeJS.Timeout | undefined = undefined;
 
 	// create a decorator type that we use to decorate secrets
@@ -20,6 +22,16 @@ export function activate(context: vscode.ExtensionContext) {
 
 	let activeEditor = vscode.window.activeTextEditor;
 
+	async function getAllowList() {
+		const url = config.allowlist_url;
+		if (url === "") {
+			return [];
+		}
+		const response = await axios.get(url);
+		// return response.data.terms;
+		return response.data;
+	}
+
 	function updateDecoration(regEx: RegExp, description: string) {
 			if (!activeEditor) {
 					return;
@@ -27,7 +39,11 @@ export function activate(context: vscode.ExtensionContext) {
 			const text = activeEditor.document.getText();
 			const SecretsFoundInCode = [];
 			let match;
+
 			while ((match = regEx.exec(text))) {
+					if (allow_list.includes(match[0])) {
+						continue;
+					}
 					const startPos = activeEditor.document.positionAt(match.index);
 					const endPos = activeEditor.document.positionAt(match.index + match[0].length);
 					const decoration = { range: new vscode.Range(startPos, endPos), hoverMessage: '**' + description + '**' };
