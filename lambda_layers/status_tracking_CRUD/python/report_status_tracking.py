1a06e2886d4e9520be333c57edb81044306884e7
wontonz-1 <danielsworkemail123@gmail.com>
2021-11-21 19:21:59 +1100

    cleaned up duplicated code

--- lambda_layers/status_tracking_CRUD/python/report_status_tracking.py
+++ lambda_layers/status_tracking_CRUD/python/report_status_tracking.py
@@ -9,44 +9,55 @@ url = "http://localhost:8000"
 # This function deletes a table specified by name
 def delete_table(table_name, dynamodb=None):
     if not dynamodb: dynamodb = boto3.resource('dynamodb', endpoint_url=url)
-    table = dynamodb.Table(table_name)
-    table.delete()
-    print(f'{table_name} table successfully deleted')
+    try:
+        table = dynamodb.Table(table_name)
+        table.delete()
+    except Exception as e:
+        print(str(e))
+        return False
+    else:
+        print(f'{table_name} table successfully deleted')
+        return True
 
 
 # This table creates the tracking table
 def create_tracking_table(dynamodb=None):
     table_name = "tracking"
     if not dynamodb: dynamodb = boto3.resource('dynamodb', endpoint_url=url)
-    dynamodb.create_table(
-        TableName=table_name,
-        KeySchema=[
-            {
-                'AttributeName': 'reportURL',
-                'KeyType': 'HASH'  # Partition key
-            },
-            {
-                'AttributeName': 'reviewerID',
-                'KeyType': 'RANGE'  # Sort key
+    try:
+        dynamodb.create_table(
+            TableName=table_name,
+            KeySchema=[
+                {
+                    'AttributeName': 'reportURL',
+                    'KeyType': 'HASH'  # Partition key
+                },
+                {
+                    'AttributeName': 'reviewerID',
+                    'KeyType': 'RANGE'  # Sort key
+                }
+            ],
+            AttributeDefinitions=[
+                {
+                    'AttributeName': 'reportURL',
+                    'AttributeType': 'S'  # string
+                },
+                {
+                    'AttributeName': 'reviewerID',
+                    'AttributeType': 'S'  # string
+                }
+            ],
+            ProvisionedThroughput={
+                'ReadCapacityUnits': 5,
+                'WriteCapacityUnits': 5
             }
-        ],
-        AttributeDefinitions=[
-            {
-                'AttributeName': 'reportURL',
-                'AttributeType': 'S'  # string
-            },
-            {
-                'AttributeName': 'reviewerID',
-                'AttributeType': 'S'  # string
-            }
-        ],
-        ProvisionedThroughput={
-            'ReadCapacityUnits': 5,
-            'WriteCapacityUnits': 5
-        }
-    )
-    print("Tracking table successfully created")
-    return True
+        )
+    except Exception as e:
+        print(str(e))
+        return False
+    else:
+        print("Tracking table successfully created")
+        return True
 
 
 # This function inserts a row into the tracking table
@@ -59,52 +70,71 @@ def insert_tracking(reportURL, reviewerID, SHA, status=False, dynamodb=None):
         'SHA': SHA,
         'tracking_status': status
     }
-    table = dynamodb.Table(table_name)
-    response = table.put_item(Item=item)
 
-    return response
+    try:
+        table = dynamodb.Table(table_name)
+        response = table.put_item(Item=item)
+    except Exception as e:
+        print(str(e))
+        return False
+    else:
+        return True
 
 
 # This function updates the tracking status of a single record associated with a given reportURL and a reviewerID
 def update_tracking_status(reportURL, reviewerID, new_status, dynamodb=None):
     table_name = "tracking"
     if not dynamodb: dynamodb = boto3.resource('dynamodb', endpoint_url=url)
-    table = dynamodb.Table(table_name)
-    response = table.update_item(
-        Key={
-            'reportURL': reportURL,
-            'reviewerID': reviewerID
-        },
-        UpdateExpression="set tracking_status=:s",
-        ExpressionAttributeValues={
-            ':s': new_status,
-        },
-        ReturnValues="UPDATED_NEW"
-    )
-    print(f'successfully updated {response["Attributes"]}')
+
+    try:
+        table = dynamodb.Table(table_name)
+        response = table.update_item(
+            Key={
+                'reportURL': reportURL,
+                'reviewerID': reviewerID
+            },
+            UpdateExpression="set tracking_status=:s",
+            ExpressionAttributeValues={
+                ':s': new_status,
+            },
+            ReturnValues="UPDATED_NEW"
+        )
+    except Exception as e:
+        print(str(e))
+        return False
+    else:
+        print(f'successfully updated {response["Attributes"]}')
+        return True
 
 
 # This function updates all record's SHA value associated with a given reportURL
 def update_SHA(reportURL, new_SHA, dynamodb=None):
     table_name = "tracking"
     if not dynamodb: dynamodb = boto3.resource('dynamodb', endpoint_url=url)
-    table = dynamodb.Table(table_name)
-    # Query all records that has the given reportURL
-    records = read_tracking_report(reportURL, dynamodb)
-    # Update each record's SHA value
-    for record in records:
-        response = table.update_item(
-            Key={
-                'reportURL': reportURL,
-                'reviewerID': record['reviewerID']
-            },
-            UpdateExpression="set SHA=:s",
-            ExpressionAttributeValues={
-                ':s': new_SHA,
-            },
-            ReturnValues="UPDATED_NEW"
-        )
-        print(f'successfully updated {response["Attributes"]}')
+
+    try:
+        table = dynamodb.Table(table_name)
+        # Query all records that has the given reportURL
+        records = read_tracking_report(reportURL, dynamodb)
+        # Update each record's SHA value
+        for record in records:
+            response = table.update_item(
+                Key={
+                    'reportURL': reportURL,
+                    'reviewerID': record['reviewerID']
+                },
+                UpdateExpression="set SHA=:s",
+                ExpressionAttributeValues={
+                    ':s': new_SHA,
+                },
+                ReturnValues="UPDATED_NEW"
+            )
+            print(f'successfully updated {response["Attributes"]}')
+    except Exception as e:
+        print(str(e))
+        return False
+    else:
+        return True
 
 
 # This function deletes all records in the tracking table associated with a given reportURL
@@ -117,19 +147,44 @@ def delete_tracking_record(reportURL, dynamodb=None):
         'ConsistentRead': True
     }
 
-    table = dynamodb.Table(table_name)
-    response = table.query(**query_params)
-    deleted_items += len(response['Items'])
+    try:
+        table = dynamodb.Table(table_name)
+        response = table.query(**query_params)
+        deleted_items += len(response['Items'])
 
-    with table.batch_writer() as batch:
-        for item in response['Items']:
-            content = {
-                'reportURL': reportURL,
-                'reviewerID': item['reviewerID']
-            }
-            batch.delete_item(Key=content)
+        with table.batch_writer() as batch:
+            for item in response['Items']:
+                content = {
+                    'reportURL': reportURL,
+                    'reviewerID': item['reviewerID']
+                }
+                batch.delete_item(Key=content)
 
-    print(f'successfully deleted {deleted_items} records')
+    except Exception as e:
+        print(str(e))
+        return False
+    else:
+        print(f'successfully deleted {deleted_items} records')
+        return True
+
+
+# This function deletes a tracking record associated with a given reportURL and a reviewerID
+def delete_tracking(reportURL, reviewerID, dynamodb=None):
+    table_name = "tracking"
+    if not dynamodb: dynamodb = boto3.resource('dynamodb', endpoint_url=url)
+    key={
+        'reportURL': reportURL,
+        'reviewerID': reviewerID
+    }
+
+    try:
+        table = dynamodb.Table(table_name)
+        response = table.delete_item(Key=key)
+    except Exception as e:
+        print(str(e))
+        return False
+    else:
+        return True
 
 
 # Get all existing tracking records with the same reportURL
@@ -143,59 +198,87 @@ def read_tracking_report(reportURL, dynamodb=None):
         'ConsistentRead': True
     }
 
-    table = dynamodb.Table(table_name)
-    response = table.query(**query_params)
-    for item in response['Items']:
-        to_return.append(item)
-    return to_return
+    try:
+        table = dynamodb.Table(table_name)
+        response = table.query(**query_params)
+        for item in response['Items']:
+            to_return.append(item)
+
+        # In case the tracking table has more than 100 records
+        while response ['LastEvaluatedKey']:
+            response = table.query(
+                **query_params,
+                ExclusiveStartKey=response['LastEvaluatedKey']
+            )
+            # append any additional records 100 by 100
+            for item in response['Items']:
+                to_return.append(item['Items'])
+    except KeyError:
+        # response ['LastEvaluatedKey'] will not exist if there is no record leftover after the first run
+        return to_return
+    except Exception as e:
+        print(str(e))
+        return False
+    else:
+        return to_return
 
 
 # This function fetches the status of tracking given a specific reportURL and a reviewerID
 def read_tracking_reviewer_status(reportURL, reviewerID, dynamodb=None):
     table_name = "tracking"
     if not dynamodb: dynamodb = boto3.resource('dynamodb', endpoint_url=url)
-    table = dynamodb.Table(table_name)
-    key = {
-        'reportURL': reportURL,
-        'reviewerID': reviewerID
-    }
-    response = table.get_item(Key=key)
-    return response['Item']
+    try:
+        table = dynamodb.Table(table_name)
+        key = {
+            'reportURL': reportURL,
+            'reviewerID': reviewerID
+        }
+        response = table.get_item(Key=key)
+    except Exception as e:
+        print(str(e))
+        return False
+    else:
+        return response['Item']
 
 
 # This function creates a secret table
 def create_secret_table(dynamodb=None):
     table_name = "secret"
     if not dynamodb: dynamodb = boto3.resource('dynamodb', endpoint_url=url)
-    dynamodb.create_table(
-        TableName=table_name,
-        KeySchema=[
-            {
-                'AttributeName': 'reportURL',
-                'KeyType': 'HASH'  # Partition key
-            },
-            {
-                'AttributeName': 'secretNum',
-                'KeyType': 'RANGE'  # Sort key
+    try:
+        dynamodb.create_table(
+            TableName=table_name,
+            KeySchema=[
+                {
+                    'AttributeName': 'reportURL',
+                    'KeyType': 'HASH'  # Partition key
+                },
+                {
+                    'AttributeName': 'secretNum',
+                    'KeyType': 'RANGE'  # Sort key
+                }
+            ],
+            AttributeDefinitions=[
+                {
+                    'AttributeName': 'reportURL',
+                    'AttributeType': 'S'  # string
+                },
+                {
+                    'AttributeName': 'secretNum',
+                    'AttributeType': 'S'  # string
+                }
+            ],
+            ProvisionedThroughput={
+                'ReadCapacityUnits': 5,
+                'WriteCapacityUnits': 5
             }
-        ],
-        AttributeDefinitions=[
-            {
-                'AttributeName': 'reportURL',
-                'AttributeType': 'S'  # string
-            },
-            {
-                'AttributeName': 'secretNum',
-                'AttributeType': 'S'  # string
-            }
-        ],
-        ProvisionedThroughput={
-            'ReadCapacityUnits': 5,
-            'WriteCapacityUnits': 5
-        }
-    )
-    print("Secret table successfully created")
-    return True
+        )
+    except Exception as e:
+        print(str(e))
+        return False
+    else:
+        print("Secret table successfully created")
+        return True
 
 
 # This function inserts a record in the secret table
@@ -209,29 +292,41 @@ def insert_secret(reportURL, secretNum, secret, secret_info, status, dynamodb=No
         'secret_info': secret_info,
         'secret_status': status
     }
-    table = dynamodb.Table(table_name)
-    response = table.put_item(Item=item)
 
-    return response
+    try:
+        table = dynamodb.Table(table_name)
+        response = table.put_item(Item=item)
+    except Exception as e:
+        print(str(e))
+        return False
+    else:
+        return True
 
 
 # This function updates the secret status of a record associated with a given reportURL and a secretNum
 def update_secret_status(reportURL, secretNum, new_status, dynamodb=None):
     table_name = "secret"
     if not dynamodb: dynamodb = boto3.resource('dynamodb', endpoint_url=url)
-    table = dynamodb.Table(table_name)
-    response = table.update_item(
-        Key={
-            'reportURL': reportURL,
-            'secretNum': secretNum
-        },
-        UpdateExpression="set secret_status=:s",
-        ExpressionAttributeValues={
-            ':s': new_status,
-        },
-        ReturnValues="UPDATED_NEW"
-    )
-    print(f'successfully updated {response["Attributes"]}')
+
+    try:
+        table = dynamodb.Table(table_name)
+        response = table.update_item(
+            Key={
+                'reportURL': reportURL,
+                'secretNum': secretNum
+            },
+            UpdateExpression="set secret_status=:s",
+            ExpressionAttributeValues={
+                ':s': new_status,
+            },
+            ReturnValues="UPDATED_NEW"
+        )
+    except Exception as e:
+        print(str(e))
+        return False
+    else:
+        print(f'successfully updated {response["Attributes"]}')
+        return True
 
 
 # This function reads all secrets that are associated with a given reportURL
@@ -245,11 +340,29 @@ def read_secret(reportURL, dynamodb=None):
         'ConsistentRead': True
     }
 
-    table = dynamodb.Table(table_name)
-    response = table.query(**query_params)
-    for item in response['Items']:
-        to_return.append(item)
-    return to_return
+    try:
+        table = dynamodb.Table(table_name)
+        response = table.query(**query_params)
+        for item in response['Items']:
+            to_return.append(item)
+
+        # In case the tracking table has more than 100 records
+        while response['LastEvaluatedKey']:
+            response = table.query(
+                **query_params,
+                ExclusiveStartKey=response['LastEvaluatedKey']
+            )
+            # append any additional records 100 by 100
+            for item in response['Items']:
+                to_return.append(item['Items'])
+    except KeyError:
+        # response ['LastEvaluatedKey'] will not exist if there is no record leftover after the first run
+        return to_return
+    except Exception as e:
+        print(str(e))
+        return False
+    else:
+        return to_return
 
 
 # This function deletes all records in the secret table associated with a given reportURL
@@ -262,20 +375,21 @@ def delete_secret_record(reportURL, dynamodb=None):
         'ConsistentRead': True
     }
 
-    table = dynamodb.Table(table_name)
-    response = table.query(**query_params)
-    deleted_items += len(response['Items'])
+    try:
+        table = dynamodb.Table(table_name)
+        response = table.query(**query_params)
+        deleted_items += len(response['Items'])
 
-    with table.batch_writer() as batch:
-        for item in response['Items']:
-            content = {
-                'reportURL': reportURL,
-                'secretNum': item['secretNum']
-            }
-            batch.delete_item(Key=content)
-
-    print(f'successfully deleted {deleted_items} records')
-
-
-if __name__ == '__main__':
-    dynamodb = boto3.resource('dynamodb', region_name='ap-southeast-2')
+        with table.batch_writer() as batch:
+            for item in response['Items']:
+                content = {
+                    'reportURL': reportURL,
+                    'secretNum': item['secretNum']
+                }
+                batch.delete_item(Key=content)
+    except Exception as e:
+        print(str(e))
+        return False
+    else:
+        print(f'successfully deleted {deleted_items} records')
+        return True
