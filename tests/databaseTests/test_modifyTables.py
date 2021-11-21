f0dc00b44110edd6e296965e6d11eb88d227bcc8
z5257531 <62084453+z5257531@users.noreply.github.com>
2021-11-21 23:39:01 +1100

    MORE DAAAAKAAAA

--- tests/databaseTests/test_modifyTables.py
+++ tests/databaseTests/test_modifyTables.py
@@ -4,13 +4,6 @@ from MT_test_helpers import gen_random_strings
 
 """
 Functions to test:
-    allowlist_modifyTables.read_repo_with_info()
-    allowlist_modifyTables.insert_new_term()
-    allowlist_modifyTables.insert_new_terms()
-    allowlist_modifyTables.insert_new_terms_with_info()
-    allowlist_modifyTables.delete_term()
-    allowlist_modifyTables.delete_repo()
-    allowlist_modifyTables.delete_table()
 """
 org_id = "123456"
 
@@ -134,6 +127,46 @@ def test_read_repo_with_info(db_resource, setup_repo):
     tterms = allowlist_modifyTables.read_repo_with_info(org_id, repo_id, dynamodb=db_resource)
     assert len(terms)+1 == (len(tterms)) 
 
+def test_delete_repo(db_resource, setup_repo):
+    tableName = f"{allowlist_modifyTables.tablename_prefix}_{org_id}"
+    table = db_resource.Table(tableName)
+    assert table.item_count == 1
+
+    allowlist_modifyTables.delete_repo(org_id, repo_id, dynamodb=db_resource)
+    table = db_resource.Table(tableName)
+    assert table.item_count == 0
+
+    allowlist_modifyTables.setup_new_repo(org_id, repo_id, dynamodb=db_resource)
+    table = db_resource.Table(tableName)
+    assert table.item_count == 1
+
+    terms = gen_random_strings(500)
+    allowlist_modifyTables.insert_new_terms(org_id, repo_id, terms, dynamodb=db_resource)
+    table = db_resource.Table(tableName)
+    assert table.item_count == len(terms) + 1
+
+    allowlist_modifyTables.delete_repo(org_id, repo_id, dynamodb=db_resource)
+    table = db_resource.Table(tableName)
+    assert table.item_count == 0
+
+def test_delete_term(db_resource, setup_repo):
+    tableName = f"{allowlist_modifyTables.tablename_prefix}_{org_id}"
+    table = db_resource.Table(tableName)
+    assert table.item_count == 1
+
+    term = "hello"
+    allowlist_modifyTables.insert_new_term(org_id, repo_id, term, dynamodb=db_resource)
+    table = db_resource.Table(tableName)
+    assert table.item_count == 2
+
+    allowlist_modifyTables.delete_term(org_id, repo_id, term, dynamodb=db_resource)
+    table = db_resource.Table(tableName)
+    assert table.item_count == 1
+
+def test_delete_table(db_resource, create_table):
+    allowlist_modifyTables.delete_table(org_id, dynamodb=db_resource)
+    tables = list(db_resource.tables.all())
+    assert len(tables) == 0
 
 def test_functions_without_setup(db_resource):
     assert True
